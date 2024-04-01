cc13ca37d5f33454e1e172782e515a4ea167af65
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2024-04-01 22:09:18 +0300

    2 commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'
+import userRoutes from './routes/users.js'
 
 const app = express();
 dotenv.config();
@@ -14,12 +15,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONECTION_URL = 'mongodb+srv://Lily:Lily1234567@cluster0.t0aohrp.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-    .catch((error) => console.log(error.message))
+    .catch((error) => console.log(error))
 
 mongoose.set()
